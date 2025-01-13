9f4e0bd91ab51eaafa2282ec3f63bf29a98d71f7
moxDroid Labs Inc <pritam.world@gmail.com>
2025-01-13 16:56:22 -0500

    file updated

--- data_examples.js
+++ data_examples.js
@@ -4,7 +4,6 @@ console.log(today) //2025-01-13T21:31:49.552Z
 //Timestamp
 var now = Date.now()
 console.log(now) //1736803909565
-
 console.log(`Day : ${today.getDate()}`)
 console.log(`Date : ${today.toDateString()}`)
 console.log(`Time : ${today.getTime()}`)
